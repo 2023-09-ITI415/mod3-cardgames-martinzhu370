3230b6f3c5550387409b129cab0aa7eea91f16a2
martinzhu370 <58714222+martinzhu370@users.noreply.github.com>
2023-12-15 20:38:52 -0500

    1

--- Assets/01-Prospector/__Scripts/CardProspector.cs
+++ Assets/01-Prospector/__Scripts/CardProspector.cs
@@ -1,25 +1,34 @@
-using UnityEngine;
-using System.Collections; 
+using System.Collections;
 using System.Collections.Generic;
-// This is an enum, which defines a type of variable that only has a few
-// possible named values. The CardState variable type has one of four values: // drawpile, tableau, target, & discard
-public enum CardState {
-drawpile, tableau, target, discard
+using UnityEngine;
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
 }
-public class CardProspector : Card { // Make sure CardProspector extends Card // This is how you use the enum CardState
-public CardState state = CardState.drawpile;
-// The hiddenBy list stores which other cards will keep this one face down
+
+
+public class CardProspector : Card { 
+
+[Header("Set Dynamically: CardProspector")]
+// This is how you use the enum eCardState
+public eCardState state = eCardState.drawpile;
+// The hiddenBy list stores which other cards will keep this one face
+
 public List<CardProspector> hiddenBy = new List<CardProspector>();
-// LayoutID matches this card to a Layout XML id if it's a tableau card
+// The layoutID matches this card to the tableau XML if it's a tableau
+
 public int layoutID;
-// The SlotDef class stores information pulled in from the LayoutXML <slot> 
+// The SlotDef class stores information pulled in from the LayoutXML
+
 public SlotDef slotDef;
-
-
-override public void OnMouseUpAsButton() {
-// Call the CardClicked method on the Prospector singleton 
-Prospector.S.CardClicked(this);
-// Also call the base class (Card.cs) version of this method 
-base.OnMouseUpAsButton(); // a
-}
+    override public void OnMouseUpAsButton()
+    {
+        // Call the CardClicked method on the Prospector singleton
+        Prospector.S.CardClicked(this);
+        // Also call the base class (Card.cs) version of this method
+        base.OnMouseUpAsButton(); // a
+    }
 }
